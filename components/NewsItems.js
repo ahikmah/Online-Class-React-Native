55cdf2fd0342e8e83d941905c4fcce4acd5d2ef4
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-05-09 13:04:42 +0700

    news api

---
+++ components/NewsItems.js
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, View, Text, Dimensions, Image} from 'react-native';
+import Carousel, {PaginationLight} from 'react-native-x2-carousel';
+import {NEWS_API, NEWS_API_KEY} from '@env';
+import axios from 'axios';
+function NewsItems() {
+  const [articles, setArticles] = useState();
+  useEffect(() => {
+    axios
+      .get(
+        `${NEWS_API}/top-headlines?country=us&category=technology&apiKey=${NEWS_API_KEY}`,
+      )
+      .then(res => setArticles(res.data.articles))
+      .catch(err => console.log(err));
+  }, []);
+  let renderItem;
+  const size = 5;
+  if (articles) {
+    renderItem = data => (
+      <View key={data.title} style={styles.item}>
+        <Image source={{uri: data.urlToImage}} style={styles.image} />
+        <View style={styles.overlay} />
+        <Text style={styles.title}>{data.title}</Text>
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <Carousel
+        pagination={PaginationLight}
+        renderItem={renderItem}
+        data={articles && articles.slice(0, size)}
+        autoplay={true}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    borderRadius: 20,
+    marginTop: 16,
+  },
+  item: {
+    width: Dimensions.get('window').width - 13,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  title: {
+    fontFamily: 'Montserrat-Bold',
+    position: 'absolute',
+    zIndex: 1,
+    fontSize: 15,
+    bottom: 0,
+    color: 'white',
+    padding: 23,
+    // paddingBottom: 20,
+  },
+  overlay: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    backgroundColor: 'rgba(0, 0, 0, 0.41)',
+    zIndex: 1,
+    top: 0,
+    borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 20,
+  },
+});
+
+export default NewsItems;
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,22 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useEffect, useState, useRef} from 'react';
-import {View, Text, StyleSheet, StatusBar, Dimensions} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  Image,
+} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import NewsItems from '../components/NewsItems';
 import {getDataUser} from '../redux/Action/auth';
 import {connect} from 'react-redux';
 import {DOMAIN_API, PORT_API} from '@env';
+import {Icon, Input, Item} from 'native-base';
 function Home(props) {
   console.log(props.auth.isLogin);
-  const [dataUser, setDataUser] = useState();
+  const [dataUser, setDataUser] = useState('');
   const ref = useRef();
 
   useEffect(() => {
@@ -25,22 +35,115 @@ function Home(props) {
   dataUser && console.log(dataUser.role);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text>{dataUser ? dataUser.role : null}</Text>
+    <>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={styles.topSection}>
+            <Text style={styles.txtGreeting}>Welcome back,</Text>
+            <Icon
+              name="notifications-sharp"
+              style={{color: 'white', fontSize: 24}}
+            />
+            <View style={styles.notifFlag} />
+          </View>
+          <Text style={styles.name}>
+            {dataUser ? dataUser.full_name.split(' ')[0] : null}
+          </Text>
+          <View style={styles.searchSection}>
+            <Item style={styles.searchInputContainer}>
+              <Icon
+                name="search-outline"
+                style={{color: 'rgba(1, 6, 32, 0.5)'}}
+              />
+              <Input
+                placeholder="Looking for something"
+                style={styles.searchInput}
+              />
+            </Item>
+          </View>
+        </View>
+        {/* <Text>{dataUser ? dataUser.role : null}</Text> */}
+        <NewsItems />
       </View>
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: StatusBar.currentHeight,
-    height: Dimensions.get('window').height + StatusBar.currentHeight,
-    backgroundColor: '#F9F9F9',
+    backgroundColor: '#E5E5E5',
   },
-  main: {
-    flex: 1,
+  header: {
+    paddingTop: StatusBar.currentHeight + 12,
+    backgroundColor: '#5784BA',
+    paddingHorizontal: 26,
+    borderBottomStartRadius: 20,
+    borderBottomEndRadius: 20,
+    paddingBottom: 24,
+  },
+  topSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  notifFlag: {
+    position: 'absolute',
+    height: 9,
+    width: 9,
+    borderRadius: 4.5,
+    backgroundColor: 'red',
+    borderColor: '#5784BA',
+    borderWidth: 1,
+    right: 1.9,
+    top: 2,
+  },
+  txtGreeting: {
+    fontFamily: 'Kanit-Medium',
+    color: 'white',
+    fontSize: 20,
+  },
+  profileSummary: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 18,
+  },
+  avatar: {
+    height: 56,
+    width: 56,
+    borderRadius: 56 / 2,
+  },
+
+  searchSection: {
+    marginTop: 29.67,
+  },
+  searchInputContainer: {
+    borderWidth: 1,
+    backgroundColor: '#E5E6EB',
+    paddingHorizontal: 10,
+    borderRadius: 15,
+  },
+  searchInput: {
+    fontFamily: 'Roboto-Medium',
+    color: 'rgba(1, 6, 32, 0.5)',
+    fontSize: 14,
+  },
+  info: {
+    marginLeft: 33,
+  },
+  name: {
+    color: 'white',
+    fontFamily: 'Kanit-Medium',
+    fontSize: 32,
+  },
+  status: {
+    color: 'white',
+    fontFamily: 'Roboto-Regular',
+    fontSize: 14,
   },
 });
 const mapStateToProps = state => ({
