5a4b7fe4dad031262e0374cbec71653065b12e58
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-05-26 02:39:11 +0700

    TODO: Fix later

--- android/app/src/main/java/com/onlineclass/MainApplication.java
+++ android/app/src/main/java/com/onlineclass/MainApplication.java
@@ -1,12 +1,15 @@
 package com.onlineclass;
 
 // com.myapp should be your package name
-import com.onlineclass.generated.BasePackageList;
+// import com.onlineclass.generated.BasePackageList;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
@@ -19,14 +22,14 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Arrays;
+// import java.util.Arrays;
  
-import org.unimodules.adapters.react.ModuleRegistryAdapter;
-import org.unimodules.adapters.react.ReactModuleRegistryProvider;
-import org.unimodules.core.interfaces.SingletonModule;
+// import org.unimodules.adapters.react.ModuleRegistryAdapter;
+// import org.unimodules.adapters.react.ReactModuleRegistryProvider;
+// import org.unimodules.core.interfaces.SingletonModule;
 
 public class MainApplication extends Application implements ReactApplication {
-  private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
+  // private final ReactModuleRegistryProvider mModuleRegistryProvider = new ReactModuleRegistryProvider(new BasePackageList().getPackageList(), null);
 
   private final ReactNativeHost mReactNativeHost =
       new ReactNativeHost(this) {
@@ -43,10 +46,14 @@ public class MainApplication extends Application implements ReactApplication {
           // packages.add(new MyReactNativePackage());
 
           // Add unimodules
-          List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
-            new ModuleRegistryAdapter(mModuleRegistryProvider)
-          );
-          packages.addAll(unimodules);
+          // List<ReactPackage> unimodules = Arrays.<ReactPackage>asList(
+          //   new ModuleRegistryAdapter(mModuleRegistryProvider)
+          // );
+          // packages.addAll(unimodules)
+
+        //  packages.add(newReactNativePushNotificationPackage());
+            
+          
           return packages;
         }
 
