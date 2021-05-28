49f9f710778257681b68b1d5286c261c29dc899e
Awaliyatul Hikmah <57889114+ahikmah@users.noreply.github.com>
2021-05-28 13:42:50 +0700

    local notification

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {useEffect} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -19,17 +20,39 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
   Header,
   LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import SplashScreen from 'react-native-splash-screen';
+import PushNotification from 'react-native-push-notification';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
+
+  const channel = 'notif';
+  useEffect(() => {
+    PushNotification.createChannel(
+      {
+        channelId: 'notif', // (required)
+        channelName: 'My Notification Channel', // (required)
+        channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
+        soundName: 'default', // (optional) See `soundName` parameter of `localNotification` function
+        importance: 4, // (optional) default: 4. Int value of the Android notification importance
+        vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+      },
+      created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+    );
+  }, []);
+
+  useEffect(() => {
+    PushNotification.getChannels(channel_ids => {
+      console.log(channel_ids); // ['channel_id_1']
+      // setChannel(channel_ids[0]);
+    });
+  }, []);
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -43,6 +66,22 @@ const App = () => {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
+        <Button
+          title="Press Me to Notif"
+          onPress={() =>
+            PushNotification.localNotification({
+              channelId: channel,
+              title: 'Hello',
+              message: 'This is notification',
+              largeIcon: 'ic_launcher',
+              smallIcon: '',
+              bigText:
+                'My big text that will be shown when notification is expanded', // (optional) default: "message" prop
+              subText: 'This is a subText', // (optional) default: none
+              // color: 'ic_notification',
+            })
+          }
+        />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
---
+++ redux/store.js
@@ -0,0 +1,19 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import {persistStore} from 'redux-persist';
+
+import rootReducer from './Reducers';
+import rpm from 'redux-promise-middleware';
+
+// const composeEnhancers =
+//   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+//     : compose;
+
+// const enhancers = composeEnhancers(
+//   applyMiddleware(rpm),
+//   // other store enhancers if any
+// );
+
+const enhancers = applyMiddleware(rpm);
+export const store = createStore(rootReducer, enhancers);
+export const persistor = persistStore(store);
