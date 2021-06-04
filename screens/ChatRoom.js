9f5ca3c4e308f8a5bc2d61eaeaf4002c4b6386d3
ahikmah <imadlz15@gmail.com>
2021-06-04 13:24:35 +0700

    Put everything in its right place

--- screens/ChatRoom.js
+++ screens/ChatRoom.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
@@ -35,45 +36,91 @@ function ChatRoom({...props}) {
             </View>
           </View>
         </View>
-        <ScrollView style={styles.chatBallon}>
+        <ScrollView
+          style={styles.chatBallon}
+          contentContainerStyle={{paddingBottom: 12}}>
           <View style={styles.sender}>
-            <Text style={styles.chatContentSender}>Helloo ini saya</Text>
+            <Text style={styles.chatContentSender}>
+              Helloo ini saya. Apakah dia mau memanjang. bagaimana kalo banyak
+              sekaliwkwkwk. Oh tidak, aku ternyata kebawah
+            </Text>
+            <View style={styles.chatInfoSender}>
+              <Text style={styles.timeStampSender}>12.45pm</Text>
+              <Icon
+                name="checkmark-outline"
+                style={{fontSize: 20, color: '#ADA9BB'}}
+              />
+            </View>
             <View style={styles.rightPoint}></View>
           </View>
+
           <View style={styles.receiver}>
-            <Text style={styles.chatContentReceiver}>Helloo ini dia</Text>
+            <Text style={styles.chatContentReceiver}>
+              Helloo ini dia.. Tes dulu ini hasiln
+            </Text>
+            <View style={styles.chatInfoReceiver}>
+              <Text style={styles.timeStampReceiver}>13.50 pm</Text>
+              <Icon
+                name="checkmark-outline"
+                style={{fontSize: 20, color: '#ADA9BB'}}
+              />
+            </View>
             <View style={styles.leftPoint}></View>
           </View>
+
           <View style={styles.sender}>
             <Text style={styles.chatContentSender}>Helloo ini saya</Text>
+            <View style={styles.chatInfoSender}>
+              <Text style={styles.timeStampSender}>12.45pm</Text>
+              <Icon
+                name="checkmark-outline"
+                style={{fontSize: 20, color: '#ADA9BB'}}
+              />
+            </View>
             <View style={styles.rightPoint}></View>
           </View>
+
           <View style={styles.receiver}>
-            <Text style={styles.chatContentReceiver}>Helloo ini dia</Text>
+            <Text style={styles.chatContentReceiver}>
+              Iyaaaa... Apakabarrr??
+            </Text>
+            <View style={styles.chatInfoReceiver}>
+              <Text style={styles.timeStampReceiver}>13.50 pm</Text>
+              <Icon
+                name="checkmark-outline"
+                style={{fontSize: 20, color: '#ADA9BB'}}
+              />
+            </View>
             <View style={styles.leftPoint}></View>
           </View>
+
           <View style={styles.sender}>
-            <Text style={styles.chatContentSender}>Helloo ini saya</Text>
+            <Text style={styles.chatContentSender}>
+              Helloo ini saya.. wkwkkw
+            </Text>
+            <View style={styles.chatInfoSender}>
+              <Text style={styles.timeStampSender}>12.45pm</Text>
+              <Icon
+                name="checkmark-outline"
+                style={{fontSize: 20, color: '#ADA9BB'}}
+              />
+            </View>
             <View style={styles.rightPoint}></View>
           </View>
+
           <View style={styles.receiver}>
-            <Text style={styles.chatContentReceiver}>Helloo ini dia</Text>
-            <View style={styles.leftPoint}></View>
-          </View>
-          <View style={styles.sender}>
-            <Text style={styles.chatContentSender}>Helloo ini saya</Text>
-            <View style={styles.rightPoint}></View>
-          </View>
-          <View style={styles.receiver}>
-            <Text style={styles.chatContentReceiver}>Helloo ini dia</Text>
-            <View style={styles.leftPoint}></View>
-          </View>
-          <View style={styles.sender}>
-            <Text style={styles.chatContentSender}>Helloo ini saya</Text>
-            <View style={styles.rightPoint}></View>
-          </View>
-          <View style={styles.receiver}>
-            <Text style={styles.chatContentReceiver}>Helloo ini dia</Text>
+            <Text style={styles.chatContentReceiver}>
+              Mau nyoba tulisan panjang apa ya.. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+              labore et dolore magna aliqua.
+            </Text>
+            <View style={styles.chatInfoReceiver}>
+              <Text style={styles.timeStampReceiver}>13.50 pm</Text>
+              <Icon
+                name="checkmark-outline"
+                style={{fontSize: 20, color: '#ADA9BB'}}
+              />
+            </View>
             <View style={styles.leftPoint}></View>
           </View>
         </ScrollView>
@@ -111,6 +158,15 @@ const styles = StyleSheet.create({
     borderBottomEndRadius: 20,
     paddingBottom: 16,
     zIndex: 2,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
   topSection: {
     flexDirection: 'row',
@@ -128,40 +184,126 @@ const styles = StyleSheet.create({
   },
 
   chatBallon: {
-    paddingHorizontal: 12,
-    paddingVertical: 12,
-    marginBottom: 12,
+    paddingHorizontal: 16,
+    // marginTop: 1,
+    marginBottom: 10,
+    paddingTop: 12,
   },
-
-  // rightPoint: {
-  //   width: 0,
-  //   height: 0,
-  //   borderLeft: 2,
-  //   borderRight: 2,
-  //   borderTop: 10,
-  //   position: 'absolute',
-  //   top: '45%',
-  //   right: '-10%',
-  //   // transform: rotate(-60),
-  // },
 
   sender: {
-    backgroundColor: 'green',
+    // flexDirection: '',
+    alignSelf: 'flex-end',
+    backgroundColor: '#5784BA',
     marginBottom: 12,
     padding: 12,
-  },
-  receiver: {
-    backgroundColor: 'orange',
-    padding: 12,
-    marginBottom: 12,
+    borderTopStartRadius: 15,
+    borderTopEndRadius: 15,
+    borderBottomStartRadius: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
 
   chatContentSender: {
+    color: '#F9F9F9',
+    fontSize: 15,
+    fontFamily: 'Roboto-Regular',
+  },
+  chatInfoSender: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  timeStampSender: {
+    marginTop: 3,
+    marginRight: 3,
+    color: '#ADA9BB',
     textAlign: 'right',
   },
 
+  receiver: {
+    alignSelf: 'flex-start',
+    backgroundColor: '#F9F9F9',
+    padding: 12,
+    borderTopStartRadius: 15,
+    borderTopEndRadius: 15,
+    borderBottomEndRadius: 15,
+    marginBottom: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  chatContentReceiver: {
+    color: '#3F4356',
+    fontSize: 15,
+    fontFamily: 'Roboto-Regular',
+  },
+  chatInfoReceiver: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+
+  timeStampReceiver: {
+    marginRight: 3,
+    color: '#ADA9BB',
+    textAlign: 'right',
+  },
+
+  rightPoint: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#5784BA',
+    position: 'absolute',
+    right: -10,
+    borderTopEndRadius: 30,
+    borderBottomStartRadius: 30,
+    bottom: -10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  leftPoint: {
+    width: 20,
+    height: 20,
+    backgroundColor: '#F9F9F9',
+    position: 'absolute',
+    left: -10,
+    borderTopStartRadius: 30,
+    borderBottomEndRadius: 30,
+    bottom: -10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
   inputSection: {
-    backgroundColor: 'yellow',
+    // backgroundColor: 'yellow',
     height: 60,
     paddingHorizontal: 12,
     paddingBottom: 12,
@@ -173,27 +315,34 @@ const styles = StyleSheet.create({
     width: '85%',
   },
   sendBtn: {
-    // top: 0,
-    height: 55,
-    width: 55,
+    backgroundColor: '#5784BA',
+    height: 50,
+    width: 50,
     borderRadius: 100,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
   },
 
   txtSend: {
-    // width: '%',
     color: 'white',
     textAlign: 'center',
-    // paddingHorizontal: 12,
+    fontSize: 20,
   },
   inputMessage: {
     textAlignVertical: 'center',
-    // paddingHorizontal: 90,
-    paddingLeft: 24,
-    paddingRight: 24,
+    paddingLeft: 20,
+    paddingRight: 20,
     borderRadius: 40,
     marginTop: 0,
     height: 55,
-    backgroundColor: 'red',
+    backgroundColor: '#F9F9F9',
     width: '100%',
   },
 });
